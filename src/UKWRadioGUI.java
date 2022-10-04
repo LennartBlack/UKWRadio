8e6766efa44f24ee0b40767caeb07d84b185693f
Lennart Schwartz <lennart.schwartz@ewe-netz.de>
2022-10-04 11:33:49 +0200

    added basic Layout for a radio

--- src/UKWRadioGUI.java
+++ src/UKWRadioGUI.java
@@ -1,4 +1,23 @@
-public class UKWRadioGUI {
-    super("UKWRadio");
+import java.awt.*;
 
+public class UKWRadioGUI extends Frame {
+    UKWRadioGUI() {
+        super("UKWRadio");
+        UKWRadio radio = new UKWRadio(2);
+        this.setLayout(new GridLayout(4, 1));
+            Label showFrequency = new Label("" + radio.deliverCurrentFrequency());
+            Panel changeF = new Panel();
+            Panel save1Panel = new Panel();
+            Panel save2Panel = new Panel();
+            this.add(showFrequency);
+            this.add(changeF);
+            this.add(save1Panel);
+            this.add(save2Panel);
+
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new UKWRadio();
+    }
 }
