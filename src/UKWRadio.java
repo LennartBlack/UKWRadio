a4687db1a42bcf2670fd0afbff3e94d39f4b854e
Lennart Schwartz <lennart.schwartz@ewe-netz.de>
2022-10-04 11:00:15 +0200

    adding methods concerning frequency and save Buttons

--- src/UKWRadio.java
+++ src/UKWRadio.java
@@ -2,9 +2,17 @@ public class UKWRadio {
     protected double minFrequency = 87.5;
     protected double maxFrequency = 108.0;
     protected double currentFrequency = 87.5;
-    protected String[] functionButtons;
+    protected double[] saveButtons;
 
     UKWRadio(int n){
-        functionButtons = new String[n];
+        saveButtons = new double[n];
     }
+    UKWRadio(){
+        saveButtons = new double[3];}
+    protected double deliverCurrentFrequency(){return currentFrequency;}
+    protected void lowerFrequency(){currentFrequency-=0.5;}
+    protected void raiseFrequency(){currentFrequency+=0.5;}
+    protected void saveFrequency(int n){
+        saveButtons[n]=currentFrequency;}
+    protected void setFrequency(int n){currentFrequency= saveButtons[n];}
 }
