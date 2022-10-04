85802a07d8d2c79553ad8781357e03dadd8fde12
Lennart Schwartz <lennart.schwartz@ewe-netz.de>
2022-10-04 10:48:57 +0200

    adding function Buttons

--- src/UKWRadio.java
+++ src/UKWRadio.java
@@ -2,5 +2,9 @@ public class UKWRadio {
     protected double minFrequency = 87.5;
     protected double maxFrequency = 108.0;
     protected double currentFrequency = 87.5;
+    protected String[] functionButtons;
 
+    UKWRadio(int n){
+        functionButtons = new String[n];
+    }
 }
